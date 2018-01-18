3f7523f7c47cd13023135358ae8f7070eead3067
maiconcodedevelopment <codedevelopment@code.com>
2018-01-18 17:58:52 -0200

    Login initial

---
+++ app/Model/LoginUser.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Model;
+
+class LoginUser{
+    protected static $guquaty = array();
+}
---
+++ resources/views/plaiConMain.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maicon
+ * Date: 17/01/18
+ * Time: 18:08
+ */
